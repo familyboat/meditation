973e01c8c5ebd9cee02113da4706b54f3e5ca9c2
familyboat <familyboat@163.com>
2023-09-01 12:19:54 +0800

    add something

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,47 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: 'notes'
+  plugins: [react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      manifest: {
+        name: "Notes",
+        short_name: "Notes",
+        description: "Make your own notes",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/notes/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/notes/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/notes/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/notes/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+    }),
+],
+  base: 'notes',
 })
